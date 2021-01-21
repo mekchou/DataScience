004e77c262452ad6d97a20c6abbcc7e9d8b2b5ed
mek <mek.chou@gmail.com>
2021-01-21 17:38:37 -0500

    satellite B video

--- Satellite_Example_B.py
+++ Satellite_Example_B.py
@@ -46,6 +46,23 @@ half_upper_mask = np.logical_and(half_upper, circular_mask)
 photo_data[half_upper_mask] = random.randint(200,255)
 plt.figure(figsize = (15,15))
 plt.imshow(photo_data)
+
+# Processing of red pixels
+photo_data = imageio.imread('./numpy_satellite/wifire/sd-3layers.jpg')
+# (r,g,b) = (0,1,2)
+red_mask = photo_data[:, :, 0] <150
+photo_data[red_mask] = 0
+plt.figure(figsize = (15,15))
+plt.imshow(photo_data)
+
+# Composite mask that takes thresholds on all three rgb layers
+photo_data = imageio.imread('./numpy_satellite/wifire/sd-3layers.jpg')
+red_mask = photo_data[:, :, 0] <150
+green_mask = photo_data[:, :, 1] > 100
+blue_mask = photo_data[:, :, 2] < 100
+final_mask = np.logical_and(red_mask, green_mask, blue_mask)
+photo_data[final_mask] = 0
+plt.figure(figsize = (15,15))
+plt.imshow(photo_data)
+
 plt.show()
-
-
